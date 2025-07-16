3d6fd1d435bfbf0915f106b31ac9bb3fac2c056a
Alifiroozi70 <al.fir.date@gmail.com>
2025-07-16 09:40:21 +0330

    Response change

--- cron_jobs/main.py
+++ cron_jobs/main.py
@@ -10,7 +10,7 @@ subprocess.run(["python", "cron_jobs/daily/update_daily_data.py"])
 subprocess.run(["python", "cron_jobs/daily/update_daily_indicator_for_All_Data.py"])
 subprocess.run(["python", "cron_jobs/daily/update_daily_haghighi.py"])
 subprocess.run(["python", "cron_jobs/daily/run_daily_join_sql.py"])
-
+subprocess.run(["python", "cron_jobs/daily/SafKharid.py"])
 # اجرای اسکریپت‌های هفتگی (فقط روز پنجشنبه مثلاً)
 subprocess.run(["python", "cron_jobs/weekly/update_weekly_data.py"])
 subprocess.run(["python", "cron_jobs/weekly/update_weekly_indicator_for_All_Data.py"])
