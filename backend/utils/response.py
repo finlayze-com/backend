d75ad93bec4b2de695d3678c3a1fe2ce8ca52925
Alifiroozi70 <al.fir.date@gmail.com>
2025-07-17 13:26:46 +0330

    1

--- backend/utils/response.py
+++ backend/utils/response.py
@@ -1,8 +1,9 @@
-def create_response(status: str, message: str = "", data: dict = None):
+def create_response(status: str, status_code: int, message: str = "", data: dict = None):
     if data is None:
         data = {}
     return {
         "status": status,
+        "status_code": status_code,
         "message": message,
         "data": data
     }
