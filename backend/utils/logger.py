3e33a9db4395d50141bf1fb47ba1b0b1c8646d2f
Alifiroozi70 <al.fir.date@gmail.com>
2025-07-21 10:00:35 +0330

    log system add

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -2,20 +2,23 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 
-# ⛳ مسیر مطلق ریشه پروژه
+# مسیر مطلق به پوشه logs
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 log_dir = os.path.join(BASE_DIR, "logs")
 os.makedirs(log_dir, exist_ok=True)
-log_file = os.path.join(log_dir, "app.log")
+
+log_file = os.path.join(log_dir, "main.log")  # 👈 تغییر از app.log به main.log
 
 # تنظیمات لاگر اصلی
 logger = logging.getLogger("app_logger")
 logger.setLevel(logging.DEBUG)
 
 # هندلر فایل با Rotation (مثلاً هر فایل 5MB، حداکثر 3 نسخه)
-file_handler = RotatingFileHandler(log_file, maxBytes=5_000_000, backupCount=3)
-file_formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
-file_handler.setFormatter(file_formatter)
+if not logger.hasHandlers():
+    file_handler = RotatingFileHandler(log_file, maxBytes=5_000_000, backupCount=3)
+    file_formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+    file_handler.setFormatter(file_formatter)
+
 
 # هندلر ترمینال (اختیاری)
 console_handler = logging.StreamHandler()
